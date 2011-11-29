f301b59b9b70c3a8b7dfc7cb825fc984867c7a80
SM <sol@archetyped.com>
2011-11-29 05:27:52 +0000

    Version 1.6.1
    General
    * Update: Version
    * Update: Changelog in readme
    * Update: Optimized JS

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Lightbox
 Plugin URI: http://archetyped.com/tools/simple-lightbox/
 Description: Customizable Lightbox for Wordpress
-Version: 1.6
+Version: 1.6.1
 Author: Archetyped
 Author URI: http://archetyped.com
 */
