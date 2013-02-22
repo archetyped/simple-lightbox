1e2af43474e837d933f13cfaf5e6cac5f3332cbf
SM <sol@archetyped.com>
2013-02-21 16:32:30 -1000

    Update version

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Lightbox
 Plugin URI: http://archetyped.happyfox.com/
 Description: Customizable Lightbox for WordPress
-Version: 2.0b4 (BETA)
+Version: 2.0b6 (BETA)
 Author: Archetyped
 Author URI: http://archetyped.com
 */
