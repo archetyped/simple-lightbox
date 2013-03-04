fec7f1711bb21e7a08ea99526af9f3035693c7f8
SM <sol@archetyped.com>
2013-03-04 11:42:00 -1000

    Add content handler collection class

---
+++ includes/class.content_handler_collection.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'class.base_collection.php';
+require_once 'class.content_handler.php';
+
+/**
+ * Content Handler Collection
+ * @package Simple Lightbox
+ * @subpackage Content Type
+ * @author Archetyped
+ */
+class SLB_Content_Type_Collection extends SLB_Base_Collection {
+	/* Configuration */
+	
+	protected $item_type = SLB_Content_Type;
+}
