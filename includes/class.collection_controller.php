c11b227cbaa484d89686d87d8d859463736cbc64
SM <sol@archetyped.com>
2013-03-04 18:15:00 -1000

    Add `Collection_Controller` class

---
+++ includes/class.collection_controller.php
@@ -0,0 +1,56 @@
+<?php
+require_once 'class.base_collection.php';
+
+/**
+ * Collection Controller
+ * @package Simple Lightbox
+ * @subpackage Collection
+ * @author Archetyped
+ */
+class SLB_Collection_Controller extends SLB_Base_Collection {
+	/* Configuration */
+	
+	protected $mode = 'sub';
+	
+	/* Properties */
+	
+	protected $parent = null;
+	
+	/* Methods */
+
+	public function __construct($parent = null) {
+		$this->set_parent($parent);
+		parent::__construct();
+	}
+	
+	/* Initialization */
+	
+	/* Parent */
+	
+	/**
+	 * Set parent instance
+	 * @param SLB_Base $parent (optional) Parent instance
+	 * @return obj Current instance
+	 */
+	protected function set_parent($parent = null) {
+		$this->parent = ( $parent instanceof SLB_Base ) ? $parent : null;
+		return $this;
+	}
+	
+	/**
+	 * Check if parent set
+	 * @return bool TRUE if parent set
+	 */
+	protected function has_parent() {
+		return ( is_object($this->get_parent()) ) ? true : false;
+	}
+	
+	/**
+	 * Retrieve parent
+	 * @uses $parent
+	 * @return null|obj Parent instance (NULL if no parent set)
+	 */
+	protected function get_parent() {
+		return $this->parent;
+	}
+}
