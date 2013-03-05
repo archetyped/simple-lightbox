7b8e7f3855b86d8ca6e338c3a5a74b048f2f0d9f
SM <sol@archetyped.com>
2013-03-04 15:14:50 -1000

    Content Handlers: Add custom `add` method

--- includes/class.content_handler_collection.php
+++ includes/class.content_handler_collection.php
@@ -5,11 +5,29 @@ require_once 'class.content_handler.php';
 /**
  * Content Handler Collection
  * @package Simple Lightbox
- * @subpackage Content Type
+ * @subpackage Content
  * @author Archetyped
  */
-class SLB_Content_Type_Collection extends SLB_Base_Collection {
+class SLB_Content_Handlers extends SLB_Base_Collection {
 	/* Configuration */
 	
 	protected $item_type = SLB_Content_Type;
+	
+	public $hook_prefix = 'content_handler';
+	
+	/* Collection Management */
+	
+	/**
+	 * Add content type handler
+	 * Accepts properties to create new handler OR previously-initialized handler instance
+	 * @param string $id Handler ID
+	 * @param array $props (optional) Handler properties
+	 */
+	public function add($id, $props = array()) {
+		$handler = ( is_string($id) ) ? new $this->item_type($id, $props) : $id;
+		if ( $handler instanceof $this->item_type ) {
+			//Add handler to collection
+			parent::add($handler);
+		}
+	}
 }
