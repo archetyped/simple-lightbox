b601a9bea969d8b309af7bbf084fb4fa446cbb6a
SM <sol@archetyped.com>
2013-05-01 17:58:18 -1000

    Update plugin metadata

--- main.php
+++ main.php
@@ -8,7 +8,7 @@ Author: Archetyped
 Author URI: http://archetyped.com
 */
 /* 
-Copyright 2011 Solomon Marchessault (sol@archetyped.com)
+Copyright 2013 Solomon Marchessault (sol@archetyped.com)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
