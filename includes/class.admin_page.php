f723749762d0258585b4df65933ebf4120fe86e7
SM <sol@archetyped.com>
2013-08-26 18:02:25 -1000

    Admin_Page: Add form submission support
    * Save options, etc.

--- includes/class.admin_page.php
+++ includes/class.admin_page.php
@@ -88,24 +88,61 @@ class SLB_Admin_Page extends SLB_Admin_View {
 		?>
 		<div class="content-wrap">
 		<?php
-		//Add meta boxes
-		$screen = get_current_screen();
-		foreach ( $content as $c ) {
-			$c->screen = $screen;
-			//Callback
-			if ( is_callable($c->callback) ) {
-				$callback = $c->callback;
-				add_meta_box($c->id, $c->title, $c->callback, $c->screen, $c->context, $c->priority, $c->callback_args);
-			} else {
-				//Let handlers build output
-				$this->util->do_action('render_content', $c->callback, $this, $c);
+			//Add meta boxes
+			$screen = get_current_screen();
+			foreach ( $content as $c ) {
+				$c->screen = $screen;
+				//Callback
+				if ( is_callable($c->callback) ) {
+					$callback = $c->callback;
+					add_meta_box($c->id, $c->title, $c->callback, $c->screen, $c->context, $c->priority, $c->callback_args);
+				} else {
+					//Let handlers build output
+					$this->util->do_action('render_content', $c->callback, $this, $c);
+				}
 			}
-		}
-		//Output meta boxes
-		do_meta_boxes($screen, $context, null);
+			//Output meta boxes
+			do_meta_boxes($screen, $context, null);
 		?>
 		</div>
 		<?php
+	}
+	
+	/**
+	 * Set required feature
+	 * @param string $feature Required feature
+	 */
+	private function _require($feature) {
+		if ( !isset($this->_required[$feature]) ) {
+			$this->_required[$feature] = true;
+		}
+		return $this;
+	}
+	
+	/**
+	 * Check if feature is required
+	 * @param string $feature Feature to check for
+	 * @return bool TRUE if feature required
+	 */
+	private function _is_require($feature) {
+		return ( isset($this->_required[$feature]) ) ? true : false;
+	}
+	
+	/**
+	 * Require form submission support
+	 * @return obj Page instance
+	 */
+	public function require_form() {
+		$this->_require('form_submit');
+		return $this;
+	}
+	
+	/**
+	 * Check if form submission is required
+	 * @return bool TRUE if form submission required
+	 */
+	private function is_require_form() {
+		return $this->_is_require('form_submit');
 	}
 	
 	/* Handlers */
@@ -124,6 +161,16 @@ class SLB_Admin_Page extends SLB_Admin_View {
 		<div class="wrap slb">
 			<?php $this->show_icon(); ?>
 			<h2><?php esc_html_e( $this->get_label('header') ); ?></h2>
+			<?php
+				//Form submission support
+				if ( $this->is_require_form() ) {
+					//Build form output
+					$form_id = $this->add_prefix('admin_form_' . $this->get_id_raw());
+					?>
+					<form id="<?php esc_attr_e($form_id); ?>" name="<?php esc_attr_e($form_id); ?>" action="" method="post">
+					<?php
+				}
+			?>
 			<div class="metabox-holder columns-2">
 				<div class="content-primary postbox-container">
 					<?php
@@ -137,6 +184,15 @@ class SLB_Admin_Page extends SLB_Admin_View {
 				</div>
 			</div>
 			<br class="clear" />
+			<?php
+				//Form submission support
+				if ( $this->is_require_form() ) {
+					submit_button();
+					?>
+					</form>
+					<?php
+				}
+			?>
 		</div>
 		<?php
 	}
