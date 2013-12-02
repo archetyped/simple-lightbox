b49f86fb26b2fbf2e3d0360ead25fb80bc951308
SM <sol@archetyped.com>
2013-12-01 18:05:27 -1000

    Organize Gruntfile.js paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,48 @@
 /*global module:false*/
 module.exports = function(grunt) {
+	var paths = {
+		make : function (seg, trailing) {
+			var args = Array.prototype.slice.call(arguments);
+			trailing = ( typeof args[args.length - 1] === 'boolean' ) ? args.pop() : true;
+			//Build path
+			var x, sep = '/';
+			var path = args.join(sep);
+			if ( trailing ) {
+				path += sep;
+			}
+			return path;
+		},
+		makeJS : function(base) {
+			return {
+				base : paths.make(base, paths.js),
+				dev : paths.make(base, paths.dev),
+				dist : paths.make(base, paths.dist)	
+			};
+		},
+		sass : 'sass',
+		css : 'css',
+		js : 'js',
+		dev : 'dev',
+		dist : 'dist',
+		client : 'client',
+		themes : 'themes'
+	};
+	
+	var files = {
+		client : {
+			sass : grunt.file.expandMapping(['**/*.scss'], paths.make(paths.client, paths.css), {
+				cwd: paths.make(paths.client, paths.sass),
+				ext: '.css'
+			})
+		}
+	};
+	
 	// Project configuration.
 	grunt.initConfig({
 		// Metadata.
 		pkg : grunt.file.readJSON('package.json'),
 		banner : '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %>\n' + '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' + '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' + ' Licensed <%= pkg.license %> */\n',
 		// Task configuration.
-		uglify : {
-			options : {
-				mangle: false,
-				report: 'min'
-			},
-			dist : {
-				files : grunt.file.expandMapping(['**/*.js'], 'client/js/dist/', {
-					cwd: 'client/js/dev/'
-				})
-			},
-		},
 		jshint : {
 			options : {
 				curly : true,
@@ -37,22 +63,33 @@ module.exports = function(grunt) {
 			gruntfile : {
 				src : 'Gruntfile.js'
 			},
-			lib_test : {
+			client : {
 				src : ['client/js/dev/**/*.js']
+			},
+			themes : {
+				src : ['themes/**/*.js']
 			}
 		},
 		qunit : {
 			files : ['test/**/*.html']
 		},
-		sass : {
-			dist : {
-				options : {
-					outputStyle : 'compressed',
-				},
-				files : grunt.file.expandMapping(['**/*.scss'], 'client/css/', {
-					cwd: 'client/sass/',
-					ext: '.css'
+		uglify : {
+			options : {
+				mangle: false,
+				report: 'min'
+			},
+			client : {
+				files : grunt.file.expandMapping(['**/*.js'], 'client/js/dist/', {
+					cwd: 'client/js/dev/'
 				})
+			},
+		},
+		sass : {
+			options : {
+				outputStyle : 'compressed',
+			},
+			client : {
+				files : files.client.sass
 			}
 		},
 		watch : {
@@ -60,17 +97,17 @@ module.exports = function(grunt) {
 				files : '<%= jshint.gruntfile.src %>',
 				tasks : ['jshint:gruntfile']
 			},
-			lib_test : {
-				files : '<%= jshint.lib_test.src %>',
-				tasks : ['jshint:lib_test']
+			client_js : {
+				files : '<%= jshint.client.src %>',
+				tasks : ['jshint:client']
 			},
-			sass : {
-				files : 'client/sass/**/*.scss',
-				tasks : ['sass']
+			client_sass : {
+				files : ['client/sass/**/*.scss'],
+				tasks : ['sass:client']
 			}
 		}
 	});
-
+	
 	// These plugins provide necessary tasks.
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
