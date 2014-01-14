10acdfdb0647dadbda0b5eaf199910c3b9fd0ed1
SM <sol@archetyped.com>
2014-01-13 18:13:53 -1000

    Template Tag Handler/UI: Optimize initialization of viewer event handlers

--- template-tags/ui/js/dev/tag.ui.js
+++ template-tags/ui/js/dev/tag.ui.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 SLB.View.extend_template_tag_handler('ui', {
 	_hooks : function() {
 		this.on('dom_init', function(ev) {
-			this.call_attribute('dom_init', ev);
+			this.call_attribute('events_init', ev);
 		});
 	},
-	dom_init: function(ev) {
+	events_init: function(ev) {
 		var v = ev.data.template.get_theme().get_viewer();
 		//Add event handlers
 		v.on('events-complete', function(ev, v) {
@@ -14,43 +14,38 @@ SLB.View.extend_template_tag_handler('ui', {
 
 			/* Close */
 			
-			var close = function() {
-				return v.close();
-			};
 			//Close button
-			v.get_theme().dom_get_tag('ui', 'close').click(close);
+			v.get_theme().dom_get_tag('ui', 'close').click(function() {
+				return v.close();
+			});
 			
 			/* Navigation */
 			
-			var nav_next = function() {
+			v.get_theme().dom_get_tag('ui', 'nav_next').click(function() {
 				v.item_next();
-			};
-			
-			var nav_prev = function() {
+			});
+			v.get_theme().dom_get_tag('ui', 'nav_prev').click(function() {
 				v.item_prev();
-			};
-			
-			v.get_theme().dom_get_tag('ui', 'nav_next').click(nav_next);
-			v.get_theme().dom_get_tag('ui', 'nav_prev').click(nav_prev);
+			});
 			
 			/* Slideshow */
 			
-			var slideshow_control = function() {
+			v.get_theme().dom_get_tag('ui', 'slideshow_control').click(function() {
 				v.slideshow_toggle();
-			};
-			
-			v.get_theme().dom_get_tag('ui', 'slideshow_control').click(slideshow_control);
+			});
 		});
 		
 		v.on('slideshow-toggle', function(ev, v) {
 			//Update slideshow control tag
 			var tags = v.get_theme().get_tags('ui', 'slideshow_control');
-			var render_tag = function(tag) {
-				tag.render(v.get_item()).done(function(r) {
-					r.tag.dom_get().html(r.output);
-				});
-			};
 			if ( tags.length ) {
+				//Renderer
+				var render_tag = function(tag) {
+					tag.render(v.get_item()).done(function(r) {
+						r.tag.dom_get().html(r.output);
+					});
+				};
+				//Process tags
 				for ( var x = 0; x < tags.length; x++ ) {
 					render_tag(tags[x]);
 				}
