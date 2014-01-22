56f29838274a600cfa436b6fb615ec28916d1a2e
SM <sol@archetyped.com>
2014-01-21 16:22:43 -1000

    Update `Base.get_parent()` documentation

--- client/js/dev/lib.core.js
+++ client/js/dev/lib.core.js
@@ -199,8 +199,9 @@ var Base = {
 	},
 	
 	/**
-	 * Get parent object
-	 * @return obj Parent object
+	 * Get parent instance
+	 * @uses `Base._parent` property
+	 * @return obj Parent instance
 	 */
 	get_parent: function() {
 		return this._parent;
