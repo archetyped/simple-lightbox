4308df08b4a5cf89cc959d55d7cd0f9b327f4e42
Archetyped <sol@archetyped.com>
2015-08-14 08:57:03 -1000

    Prep release (2.5.0)

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Lightbox
 Plugin URI: http://archetyped.com/tools/simple-lightbox/
 Description: The highly customizable lightbox for WordPress
-Version: 0.0.0-dev (BETA)
+Version: 2.5.0
 Author: Archetyped
 Author URI: http://archetyped.com
 Support URI: https://github.com/archetyped/simple-lightbox/wiki/Feedback-&-Support
