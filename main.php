d491e5273907ca2e9ba610dd427f360a7c09379d
Archetyped <sol@archetyped.com>
2015-09-24 16:39:41 -1000

    Prep release (2.5.2)

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Simple Lightbox
 Plugin URI: http://archetyped.com/tools/simple-lightbox/
 Description: The highly customizable lightbox for WordPress
-Version: 0.0.0-dev
+Version: 2.5.2
 Text Domain: simple-lightbox
 Domain Path: /l10n
 Author: Archetyped
