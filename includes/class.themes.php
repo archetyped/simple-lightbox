1bb57ca68f63849d9ec1c00d3b5e538d3353739d
Archetyped <sol@archetyped.com>
2018-06-06 16:16:11 -1000

    Optimize: Refactor `create_function()` to anonymous function in `Themes::opt_get_field_values()`

--- includes/class.themes.php
+++ includes/class.themes.php
@@ -279,7 +279,9 @@ class SLB_Themes extends SLB_Collection_Controller {
 		}
 		
 		// Sort themes by name
-		uasort($items, create_function('$a,$b', 'return strcmp($a->get_name(), $b->get_name());'));
+		uasort( $items, function( $a, $b ) {
+			return strcmp( $a->get_name(), $b->get_name() );
+		});
 		
 		// Insert default theme at top of array
 		if ( isset($itm_d) ) {
